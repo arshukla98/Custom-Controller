26f82d4ce94ea121a07f1f17d953619128b188af
arshukla98 <arshukla@msystechnologies.com>
2024-01-03 02:35:49 -0500

    Define Go Schema for CR

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/arshukla98/sample-controller
+
+go 1.21.5
---
+++ pkg/apis/upgrade/v1/doc.go
@@ -0,0 +1,6 @@
+// +k8s:deepcopy-gen=package
+// +groupName=upgrade.k8s.io
+
+// Package v1 is the v1 version of the API.
+package v1 // import "github.com/arshukla98/sample-controller/pkg/apis/upgrade/v1"
+
